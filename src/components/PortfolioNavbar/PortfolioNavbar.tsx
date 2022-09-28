5c4b67520dacbe1504aba6e5153490703261a8ef
Kevin Messiaen <contact@kevin-messiaen.fr>
2022-09-28 13:04:34 +0700

    ✨ Added social links

--- src/components/PortfolioNavbar/PortfolioNavbar.tsx
+++ src/components/PortfolioNavbar/PortfolioNavbar.tsx
@@ -1,16 +1,23 @@
-import {Avatar, Navbar, Text} from '@nextui-org/react';
+import {Button, Link, Navbar, User} from '@nextui-org/react';
+import {FaGithub} from 'react-icons/fa';
 
 const PortfolioNavbar = () => {
     return (
         <Navbar>
             <Navbar.Brand>
-                <Avatar
-                    squared
-                    src="avatar.jpeg"/>
-                <Text b color="inherit" hideIn="xs">
-                    Kevin.Messiaen
-                </Text>
+                <User
+                    src="avatar.jpeg"
+                    name="Kevin Messiaen"
+                />
             </Navbar.Brand>
+            <Navbar.Content>
+                <Navbar.Item>
+                    <Button auto flat iconRight={<FaGithub/>}
+                            as={Link} href="https://github.com/kevinmessiaen/" target="_blank">
+                        GitHub
+                    </Button>
+                </Navbar.Item>
+            </Navbar.Content>
         </Navbar>
     )
 }
--- src/layouts/Introduction/Introduction.tsx
+++ src/layouts/Introduction/Introduction.tsx
@@ -1,11 +1,33 @@
-import {Container, Text} from '@nextui-org/react';
+import {Button, Container, CSS, Spacer, Text} from '@nextui-org/react';
 import {useTypeAnimation} from '../../effects';
+import {MdMail} from 'react-icons/md';
+import {BsLinkedin, BsSignal} from 'react-icons/bs';
 
 const Introduction = () => {
     const name = useTypeAnimation("Kevin Messiaen");
 
+    let fullpageIntroduction: CSS = {
+        height: 'calc(100vh - var(--nextui--navbarHeight))',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center'
+    };
     return (
-        <Container css={{height: 'calc(100vh - var(--nextui--navbarHeight))'}}>
+        <Container css={fullpageIntroduction}>
+            <Container css={{d: 'flex', flexWrap: 'nowrap'}}>
+                <Button aria-label='LinkedIn' href='https://www.linkedin.com/in/kevin-messiaen/' target="_blank"
+                        css={{background: '#0A66C2'}} auto icon={<BsLinkedin/>}>
+                </Button>
+                <Spacer x={2}/>
+                <Button aria-label='Send me an email' href='mailto:kevind.messiaen@gmail.com' target="_blank"
+                        css={{background: '#EA4335'}} auto icon={<MdMail/>}>
+                </Button>
+                <Spacer x={2}/>
+                <Button aria-label='Call me on Signal' href='https://signal.me/#p/+33669177855' target="_blank"
+                        css={{background: '#3A76F0'}} auto icon={<BsSignal/>}>
+                </Button>
+            </Container>
+            <Spacer y={2}/>
             <Text h2>
                 Hello! My name is
             </Text>
